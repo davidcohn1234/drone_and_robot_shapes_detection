3787952a8fe3fa07e5f5a5d1feb9e66424b7c87e
david <david.cohn.1234@gmail.com>
2022-12-17 20:46:52 +0200

    changed input and output folders in drone/script_copy_input_folders.py

--- drone/script_copy_input_folders.py
+++ drone/script_copy_input_folders.py
@@ -1,8 +1,8 @@
 import common_utils
 
 def main():
-    input_main_folder_full_path = './input_data_simon1'
-    output_main_folder_full_path = './input_data_simon'
+    input_main_folder_full_path = './input_data1'
+    output_main_folder_full_path = './input_data'
     common_utils.copy_folders(input_main_folder_full_path, output_main_folder_full_path)
     david = 5
 
